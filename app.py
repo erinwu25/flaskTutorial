d53c1ee2f3907058bdb6f9d5793ff3f25f89cd8b
Erin Wu <erinwu@Erins-MacBook-Pro.local>
2019-06-28 16:56:21 -0700

    most of the tutorial up to variables

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Flask App!"
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/members")
+def members():
+    return "Members"
+
+@app.route("/hello/<string:name>/")
+def getMember(name):
+    return render_template('test.html', name=name)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
